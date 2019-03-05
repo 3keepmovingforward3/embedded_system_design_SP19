c986c69b1f03a6d29bea60f76964134628311c08
Benjamin Blouin <tug78140@temple.edu>
2019-03-05 10:13:41 -0500

    Update sinlookuptable.m

--- FIR DIGITAL FILTER/sinlookuptable.m
+++ FIR DIGITAL FILTER/sinlookuptable.m
@@ -1,9 +1,9 @@
 funcstr = 'sin(2*pi*x)'; % Define the sine function
-xmin = 0; % Set the minimum input of interest
-xmax = 1; % Set the maximum input of interest
-xdt = ufix(16); % Set the x data type
+xmin = 0.0; % Set the minimum input of interest
+xmax = 1.0; % Set the maximum input of interest
+xdt = fixdt(0,16); % Set the x data type
 xscale = 2^-16; % Set the x data scaling
-ydt = sfix(16); % Set the y data type
+ydt = fixdt(1,16); % Set the y data type
 yscale = 2^-14; % Set the y data scaling
 rndmeth = 'Floor'; % Set the rounding method
 nptsmax = 4096; % Specify the maximum number of points
