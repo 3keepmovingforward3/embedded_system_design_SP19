e88e2c9bf7f5d93bac9bcab44811ec68ebce29b8
Benjamin Blouin <tug78140@temple.edu>
2019-03-05 09:16:46 -0500

    Create sin lookup table script, WIP

---
+++ FIR DIGITAL FILTER/sinlookuptable.m
@@ -0,0 +1,15 @@
+funcstr = 'sin(2*pi*x)'; % Define the sine function
+xmin = 0; % Set the minimum input of interest
+xmax = 1; % Set the maximum input of interest
+xdt = ufix(16); % Set the x data type
+xscale = 2^-16; % Set the x data scaling
+ydt = sfix(16); % Set the y data type
+yscale = 2^-14; % Set the y data scaling
+rndmeth = 'Floor'; % Set the rounding method
+nptsmax = 4096; % Specify the maximum number of points
+
+[xdata, ydata, errworst] = fixpt_look1_func_approx(funcstr, ...
+xmin,xmax,xdt,xscale,ydt,yscale,rndmeth,[],nptsmax);
+
+fixpt_look1_func_plot(xdata,ydata,funcstr,xmin,xmax,xdt, ...
+xscale,ydt,yscale,rndmeth);
