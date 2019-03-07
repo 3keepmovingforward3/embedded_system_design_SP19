3cd72bd9c66c0abba1b43b2ce6780ba65413456e
Benjamin Blouin <tug78140@temple.edu>
2019-03-07 09:40:33 -0500

    removed main-1.c/updated working main.c sine wave

--- FIR DIGITAL FILTER/main.c
+++ FIR DIGITAL FILTER/main.c
@@ -1,111 +1,1 @@
-/****************************************************************************/
-/*                                                                          */
-/*  main.c      --      Definition for Pmod DA1 demo                        */
-/*                                                                          */
-/****************************************************************************/
-/*  Author:     Jon Peyron                                                  */
-/*  Copyright 2017, Digilent Inc.                                           */
-/****************************************************************************/
-/*  File Description:                                                       */
-/*  This file defines a demonstration for the use of the Pmod DA1 ip        */
-/*  core. A 3V peak-to-peak triangle wave of an unspecified frequency is    */
-/*  output from the first digital to analog converter (pins A1 and B1)      */
-/*                                                                          */
-/*  to connect to UART, use a serial terminal such as TeraTerm attached to  */
-/*  the programmed FPGA's port at the proper baud rate                      */
-/*                                                                          */
-/*   UART TYPE   BAUD RATE                                                  */
-/*   uartns550   9600                                                       */
-/*   uartlite    Configurable only in HW design                             */
-/*   ps7_uart    115200 (configured by bootrom/bsp)                         */
-/*                                                                          */
-/****************************************************************************/
-/*                                                                          */
-/*  Revision History:                                                       */
-/*  06/20/2016(JonP): Created                                               */
-/*  04/28/2017(ArtVVB): validated                                           */
-/*                                                                          */
-/****************************************************************************/
-
-#include "PmodDA1.h"
-#include "xparameters.h"
-#include "xil_cache.h"
-#include "xil_printf.h"
-
-void DemoInitialize();
-void DemoRun();
-void DemoCleanup();
-void EnableCaches();
-void DisableCaches();
-
-PmodDA1 myDevice;
-
-int main(void)
-{
-    DemoInitialize();
-    DemoRun();
-    DemoCleanup();
-    return 0;
-}
-
-void DemoInitialize()
-{
-	EnableCaches();
-    DA1_begin(&myDevice, XPAR_PMODDA1_0_AXI_LITE_SPI_BASEADDR);
-}
-
-void DemoRun()
-{
-    float dMaxValue = 3.0;
-    float dMinValue = 0.0;
-    float dStep = 0.05;
-    float dValue;
-
-    xil_printf("Starting Pmod DA1 demo...\n\r");
-
-    while(1)
-    {
-        // increase physical value from minimum to maximum value
-        for(dValue = dMinValue; dValue <= dMaxValue; dValue += dStep)
-        {
-            // send value to the DA converter
-            DA1_WritePhysicalValue(&myDevice, dValue);
-        }
-
-        // decrease physical value from maximum to minimum value
-        for(dValue = dMaxValue; dValue >= dMinValue; dValue -= dStep)
-        {
-            // send value to the DA converter
-            DA1_WritePhysicalValue(&myDevice, dValue);
-        }
-    }
-}
-
-void DemoCleanup() {
-    DA1_end(&myDevice);
-    DisableCaches();
-}
-
-void EnableCaches()
-{
-#ifdef __MICROBLAZE__
-#ifdef XPAR_MICROBLAZE_USE_ICACHE
-    Xil_ICacheEnable();
-#endif
-#ifdef XPAR_MICROBLAZE_USE_DCACHE
-    Xil_DCacheEnable();
-#endif
-#endif
-}
-
-void DisableCaches()
-{
-#ifdef __MICROBLAZE__
-#ifdef XPAR_MICROBLAZE_USE_DCACHE
-    Xil_DCacheDisable();
-#endif
-#ifdef XPAR_MICROBLAZE_USE_ICACHE
-    Xil_ICacheDisable();
-#endif
-#endif
-}
+// main.c DA2PmodTest	ECE3622// Dennis Silage   c2019#include "DA2Pmod.h"#include "xparameters.h"#include "xil_io.h"#include "xil_printf.h"#include <stdio.h>#include <math.h>#include <stdint.h>#include <limits.h>//DAC2Pmod from Address Editor in Vivado#define DA2acq  0x43C00000   //DA2 acquisition    - output#define DA2dav	0x43C00004	 //DA2 data available - input#define DA2dat1	0x43C00008	 //DA2 channel 1 data - output#define DA2dat2 0x43C0000C	 //DA2 channel 2 data - outputconst int LUTSIZE = 4096;inline void SineLUT(int16_t  SINLUT[LUTSIZE]);int main(void){    int16_t  SINLUT[LUTSIZE]; //DAC SINE LUT ARRAY DEFINITION    SineLUT(SINLUT);	//FILL DAC SINE LUT    int dacdata=0;	//LUT index    int dacdav;		//DAC data available    int dacacq=0;	//DAC acquire    Xil_Out32(DA2acq,0);    while(1)    {        if(dacdata==4096)            dacdata=0;        dacdav=Xil_In32(DA2dav);        if(dacdav==0 && dacacq==0)			//DAC not in use?        {            Xil_Out32(DA2dat1, SINLUT[dacdata]);	//output DAC data            Xil_Out32(DA2dat2, SINLUT[dacdata]);	//output DAC data            Xil_Out32(DA2acq,1);			//DAC acquire            dacacq=1;        }        if(dacdav==1 && dacacq==1)			//DAC data available        {            Xil_Out32(DA2acq,0);			//stop DAC acquire            dacacq=0;            dacdata++;						//increment DAC data        }    }}void SineLUT(int16_t  SINLUT[LUTSIZE]) {    for (int i = 0; i <= 4096; i++)    {        SINLUT[i] = (int16_t) 2000*sinf(2.0f * M_PI * ((float) i / LUTSIZE))+2092;        printf ("%d\n", SINLUT[i]);    }}
