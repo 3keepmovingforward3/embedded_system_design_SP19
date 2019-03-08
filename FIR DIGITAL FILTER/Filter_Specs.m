2a2cb443497ec3747099f5fcbc451cb11d3c32cc
Benjamin Blouin <tug78140@temple.edu>
2019-03-08 15:19:46 -0500

    added pics, updated matlab script

--- FIR DIGITAL FILTER/Filter_Specs.m
+++ FIR DIGITAL FILTER/Filter_Specs.m
@@ -1,10 +1,16 @@
 %% FIR Digital Filter
 num = 1;
 den=[1 3 3 1 4];
+[u,t]=gensig('sin',0.00676);
+H=tf(num,den);
 sys = filt(num,den,2E-5);
 isstable(sys)
 damp(sys)
 figure(1)
-bode(sys)
+options = bodeoptions;
+options.FreqUnits = 'Hz';
+bode(sys,options)
 figure(2)
 pzplot(sys)
+figure(3)
+lsim(H,u,t)
