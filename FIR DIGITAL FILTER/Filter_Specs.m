e960b8ea5e19848bf9ba3b6fc4c629a344b97a0a
Benjamin Blouin <tug78140@temple.edu>
2019-03-19 10:46:02 -0400

    better

--- FIR DIGITAL FILTER/Filter_Specs.m
+++ FIR DIGITAL FILTER/Filter_Specs.m
@@ -1,21 +1,37 @@
 %% FIR Digital Filter
-den = [1 0.25];
+% Filter Coefficients
+den = 1;
 num=[1 3 3 1 4];
+Ts=2E-5;
+
+% Original Sine Wave
 t=linspace(0,1,4096);
-H=tf(num,den);
-sys = filt(num,den,2E-5);
-isstable(sys)
 x=2000*sin(2*pi*t)+2000;
+
+% Transfer Function and Filtering System
+H=tf(num,den);
+sys = filt(num,den,Ts);
+
+% Filtering the oringal sine wave
+
+% Quick check if stable
+isstable(sys)
+
+%% Plotting Secion
+snapnow
 figure(1)
 options = bodeoptions;
 options.FreqUnits = 'Hz';
 bode(sys,options)
+title('Bode Plot');
+
+
 figure(2)
 pzplot(sys)
 figure(3)
-freqz(num,den,4096)
+freqz(num,den,4097)
 
-y = filter_for_mew(num,den,x);
+y = filter(num,den,x);
 
 figure(4)
 plot(t,x)
