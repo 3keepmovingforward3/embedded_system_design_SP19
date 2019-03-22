da0b5a71f95950d9184ff07e07afd57c74525c97
Benjamin Blouin <tug78140@temple.edu>
2019-03-22 18:08:15 -0400

    noice

---
+++ introToRTOS/introToRTOS.sdk/introToRTOS/Debug/src/freertos_hello_world.d
@@ -0,0 +1,83 @@
+src/freertos_hello_world.o src/freertos_hello_world.o: \
+ ../src/freertos_hello_world.c \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/FreeRTOS.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/projdefs.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/portable.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/deprecated_definitions.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/portmacro.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/mpu_wrappers.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/task.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/list.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/queue.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/timers.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/task.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../introToRTOS_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/FreeRTOS.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/projdefs.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/portable.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/deprecated_definitions.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/portmacro.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/mpu_wrappers.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/task.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/list.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/queue.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/timers.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/task.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../introToRTOS_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
---
+++ introToRTOS/introToRTOS.sdk/task_management/Debug/src/freertos_task_management.d
@@ -0,0 +1,68 @@
+src/freertos_task_management.o src/freertos_task_management.o: \
+ ../src/freertos_task_management.c \
+ ../../task_management_bsp/ps7_cortexa9_0/include/FreeRTOS.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/projdefs.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/portable.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/deprecated_definitions.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/portmacro.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/mpu_wrappers.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/task.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/list.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../task_management_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+../../task_management_bsp/ps7_cortexa9_0/include/FreeRTOS.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/projdefs.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/portable.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/deprecated_definitions.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/portmacro.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/mpu_wrappers.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/task.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/list.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../task_management_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
